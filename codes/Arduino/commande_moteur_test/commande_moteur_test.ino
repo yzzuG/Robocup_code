3e81eadb65a6ba6f981a166a5f14e77e0f428a52
yzzuG <armand.guzzonato@gmail.com>
2022-12-12 19:37:50 +0100

    V1

--- codes/Arduino/commande_moteur_test/commande_moteur_test.ino
+++ codes/Arduino/commande_moteur_test/commande_moteur_test.ino
@@ -19,16 +19,17 @@
 #define PIN_SENS_M3 24
 //moteur 4 (derriere droit)
 #define PIN_U_M4 5
-#define PIN_SENS_4 25
+#define PIN_SENS_M4 25
 
 //  moteur + roue
 #define REDUCTION_ROUE 3
 #define RAYON_ROUE 2.75
+//coef de prop du moteur en rad/s/V
+#define SCALING 31.42
 
 //autres
 #define CCW 1
 #define CW 0
-
 
 /********************************************************/
 /*    Variables    */
@@ -41,48 +42,37 @@ double alpha = 0 ;   // angle du cap dans le repere robot en degree
 
 // donnees sortie
 double u1, u2, u3, u4;
+double rapport1, rapport2, rapport3, rapport4;
 double sens_m1 = 0, sens_m3 = 0;
 double sens_m2 = 1, sens_m4 = 1;
 
 
+
+
+
 void setup()
 {
-  /********************************************************/
-  /*    Variables    */
-  /********************************************************/
-  double omega1, omega2, omega3, omega4;  //Variables vitesse de rotation
-
-  // donnees dentree
-  double v_cap ;  // norme du cap m/s
-  double alpha = 0 ;   // angle du cap dans le repere robot en degree
-
-  // donnees sortie
-  double u1, u2, u3, u4;
-  double rapport1,rapport2,rapport3,rapport4;
-  double sens_m1 = 0, sens_m3 = 0;
-  double sens_m2 = 1, sens_m4 = 1;
-
   //Initialisation Pins
-  pinMode(PIN_U_M1,OUTPUT);
-  pinMode(PIN_U_M2,OUTPUT);
-  pinMode(PIN_U_M3,OUTPUT);
-  pinMode(PIN_U_M4,OUTPUT);
-  
-  pinMode(PIN_SENS_M1,OUTPUT);
-  pinMode(PIN_SENS_M2,OUTPUT);
-  pinMode(PIN_SENS_M3,OUTPUT);
-  pinMode(PIN_SENS_M4,OUTPUT);
-  
-  //mise a 0 des pins
-  analogWrite(PIN_U_M1,0);
-  analogWrite(PIN_U_M2,0);
-  analogWrite(PIN_U_M3,0);
-  analogWrite(PIN_U_M4,0);
+  pinMode(PIN_U_M1, OUTPUT);
+  pinMode(PIN_U_M2, OUTPUT);
+  pinMode(PIN_U_M3, OUTPUT);
+  pinMode(PIN_U_M4, OUTPUT);
 
-  digitalWrite(PIN_SENS_M1,LOW);
-  digitalWrite(PIN_SENS_M2,LOW);
-  digitalWrite(PIN_SENS_M3,LOW);
-  digitalWrite(PIN_SENS_M4,LOW);
+  pinMode(PIN_SENS_M1, OUTPUT);
+  pinMode(PIN_SENS_M2, OUTPUT);
+  pinMode(PIN_SENS_M3, OUTPUT);
+  pinMode(PIN_SENS_M4, OUTPUT);
+
+  //mise a 0 des pins
+  analogWrite(PIN_U_M1, 0);
+  analogWrite(PIN_U_M2, 0);
+  analogWrite(PIN_U_M3, 0);
+  analogWrite(PIN_U_M4, 0);
+
+  digitalWrite(PIN_SENS_M1, LOW);
+  digitalWrite(PIN_SENS_M2, LOW);
+  digitalWrite(PIN_SENS_M3, LOW);
+  digitalWrite(PIN_SENS_M4, LOW);
 
   alpha = 0;
   v_cap = 3;
@@ -92,7 +82,7 @@ void setup()
 void loop() {
   // ENTREE
   /********************************************************/
-  
+
 
   /********************************************************/
 
@@ -100,23 +90,59 @@ void loop() {
   //CALCUL
   /********************************************************/
 
-  //a calculer en fonction de la doc moteur
 
-  omega1 = (1 / RAYON_ROUE) * v_cap * cos((45 - alpha) * PI / 180);
-  u1 = R_MOTEUR / K_COURANT_MOTEUR * ((J_EQ + K_COURANT_MOTEUR * K_VITESSE_MOTEUR / R_MOTEUR - C_RES_MOTEUR) * omega1);
-  rapport1 = map(u1,0,5,0,255);
+  //Calculs des tensions a delivree aux moteurs
+  ///////////////////moteur 1
+  omega1 = (1 / RAYON_ROUE) * v_cap * cos((45 - alpha) * PI / 180) * REDUCTION_ROUE;
+  u1 = SCALING * omega1;
+  if (u1 > 5)
+  {
+    u1 = 5;
+  }
+  else if ((u1 > 0.17) && (u1 < 0.33))
+  {
+    u1 = 0.25;
+  }
+  rapport1 = map(u1, 0, 5, 0, 255);
 
-  omega2 = (1 / RAYON_ROUE) * v_cap * cos((alpha + 45) * PI / 180);
-  u2 = R_MOTEUR / K_COURANT_MOTEUR * ((J_EQ + K_COURANT_MOTEUR * K_VITESSE_MOTEUR / R_MOTEUR - C_RES_MOTEUR) * omega2);
-  rapport2 = map(u2,0,10,0,255);
+  ///////////////////moteur 2
+  omega2 = (1 / RAYON_ROUE) * v_cap * cos((alpha + 45) * PI / 180) * REDUCTION_ROUE;
+  u2 = SCALING * omega2;
+  if (u2 > 5)
+  {
+    u2 = 5;
+  }
+  else if ((u2 > 0.17) && (u2 < 0.33))
+  {
+    u2 = 0.25;
+  }
+  rapport2 = map(u2, 0, 5, 0, 255);
 
-  omega3 = (1 / RAYON_ROUE) * v_cap * cos((45 - alpha) * PI / 180);
-  u3 = R_MOTEUR / K_COURANT_MOTEUR * ((J_EQ + K_COURANT_MOTEUR * K_VITESSE_MOTEUR / R_MOTEUR - C_RES_MOTEUR) * omega3);
-  rapport3 = map(u3,0,10,0,255);
-  
-  omega4 = (1 / RAYON_ROUE) * v_cap * cos((alpha + 45) * PI / 180);
-  u4 = R_MOTEUR / K_COURANT_MOTEUR * ((J_EQ + K_COURANT_MOTEUR * K_VITESSE_MOTEUR / R_MOTEUR - C_RES_MOTEUR) * omega4);
-  rapport4 = map(u4,0,10,0,255);
+  ///////////////////moteur 3
+  omega3 = (1 / RAYON_ROUE) * v_cap * cos((45 - alpha) * PI / 180) * REDUCTION_ROUE;
+  u3 = SCALING * omega3;
+  if (u3 > 5)
+  {
+    u3 = 5;
+  }
+  else if ((u3 > 0.17) && (u3 < 0.33))
+  {
+    u3 = 0.25;
+  }
+  rapport3 = map(u3, 0, 5, 0, 255);
+
+  ///////////////////moteur 4
+  omega4 = (1 / RAYON_ROUE) * v_cap * cos((alpha + 45) * PI / 180) * REDUCTION_ROUE;
+  u4 = SCALING * omega4;
+  if (u4 > 5)
+  {
+    u4 = 5;
+  }
+  else if ((u4 > 0.17) && (u4 < 0.33))
+  {
+    u4 = 0.25;
+  }
+  rapport4 = map(u4, 0, 5, 0, 255);
 
   /********************************************************/
 
@@ -124,15 +150,34 @@ void loop() {
   //SORTIE
   /********************************************************/
 
-  analogWrite(PIN_U_M1,rapport1);
-  analogWrite(PIN_U_M2,rapport2);
-  analogWrite(PIN_U_M3,rapport3);
-  analogWrite(PIN_U_M4,rapport4);
+  analogWrite(PIN_U_M1, rapport1);
+  analogWrite(PIN_U_M2, rapport2);
+  analogWrite(PIN_U_M3, rapport3);
+  analogWrite(PIN_U_M4, rapport4);
 
-  digitalWrite(PIN_SENS_M1,sens_m1);
-  digitalWrite(PIN_SENS_M2,sens_m2);
-  digitalWrite(PIN_SENS_M3,sens_m3);
-  digitalWrite(PIN_SENS_M4,sens_m4);
+  digitalWrite(PIN_SENS_M1, sens_m1);
+  digitalWrite(PIN_SENS_M2, sens_m2);
+  digitalWrite(PIN_SENS_M3, sens_m3);
+  digitalWrite(PIN_SENS_M4, sens_m4);
 
   /********************************************************/
+}
+
+
+double tension_moteur_impair(double alpha, double v_cap)
+{
+  double omega, u;
+
+  omega = (1 / RAYON_ROUE) * v_cap * cos((45 - alpha) * PI / 180) * REDUCTION_ROUE;
+  u = SCALING * omega;
+  if (u > 5)
+  {
+    u = 5;
+  }
+  else if ((u > 0.17) && (u < 0.33))
+  {
+    u = 0.25;
+  }
+
+  return u;
 }
